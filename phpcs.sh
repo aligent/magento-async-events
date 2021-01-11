b64db9138dd56f2ee99455823d33e8fb1353beb1
Matthew O'Loughlin <matthew.oloughlin@aligent.com.au>
2021-01-11 21:32:06 +1030

    Update phpcs runner to use the binary installed within the module vendor directory

--- phpcs.sh
+++ phpcs.sh
@@ -4,6 +4,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-../../../../vendor/bin/phpcs -s --standard=Magento2 Api Model Service \
+./vendor/bin/phpcs -s --standard=Magento2 Api Model Service \
 	&& echo -e "${GREEN}No PHP code sniff violations found${NC}"
 
