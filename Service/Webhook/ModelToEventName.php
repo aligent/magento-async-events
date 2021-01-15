62a5c1116c78bcc3d5b86103bec833411c1ddcc7
Gowri <gowri.sankar@aligent.com.au>
2021-01-15 14:03:04 +1030

    chore: new models persisted suffixed with _created instead of _updated

--- Service/Webhook/ModelToEventName.php
+++ Service/Webhook/ModelToEventName.php
@@ -8,6 +8,10 @@ class ModelToEventName
 {
     public function translate(AbstractModel $model)
     {
+        if ($model->isObjectNew()) {
+            return $model->getEventPrefix() . '_created';
+        }
+
         return $model->getEventPrefix() . '_updated';
     }
 }
