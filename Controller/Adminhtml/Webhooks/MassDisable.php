674eca0d4c602b52f1174fbdb477a2deff778b67
Gowri <gowri.sankar@aligent.com.au>
2021-01-19 14:26:10 +1030

    feat: added admin ui to view webhook subscribers
    * might have to change from webhooks_webhooks_listing to
    webhooks_subscriber_listing

---
+++ Controller/Adminhtml/Webhooks/MassDisable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aligent\Webhooks\Controller\Adminhtml\Webhooks;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+
+class MassDisable extends Action implements HttpPostActionInterface
+{
+
+    public function execute()
+    {
+
+    }
+}
