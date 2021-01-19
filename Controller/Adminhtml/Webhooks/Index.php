9cb360afa6c93d910f0c3c87e374584d1a31099e
Gowri <gowri.sankar@aligent.com.au>
2021-01-19 11:08:55 +1030

    feat: added basic admin page

---
+++ Controller/Adminhtml/Webhooks/Index.php
@@ -0,0 +1,50 @@
+<?php
+namespace Aligent\Webhooks\Controller\Adminhtml\Webhooks;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'Aligent_Webhooks::integrations_webhooks';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('Webhooks'));
+
+        return $resultPage;
+    }
+}
