f2537d31d6ca1dc062b69bd4dec1d66ad829cdf7
Gowri <gowri.sankar@aligent.com.au>
2021-08-31 20:52:10 +0930

    chore: configure main retry queue

---
+++ Model/RetryHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Aligent Consulting
+ * Copyright (c) Aligent Consulting (https://www.aligent.com.au)
+ */
+
+declare(strict_types=1);
+
+namespace Aligent\Webhooks\Model;
+
+class RetryHandler
+{
+
+}
