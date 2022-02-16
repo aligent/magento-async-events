f9244fa9a8612e974f7e577063b8f49dc77fd1fe
Gowri <gowrizrh@gmail.com>
2022-02-16 17:10:37 +1030

    fix: remove (encrypted) verification token being exposed via the API

--- Api/Data/AsyncEventSearchResultsInterface.php
+++ Api/Data/AsyncEventSearchResultsInterface.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Aligent\AsyncEvents\Api\Data;
 
 use Magento\Framework\Api\SearchResultsInterface;
@@ -8,12 +7,12 @@ use Magento\Framework\Api\SearchResultsInterface;
 interface AsyncEventSearchResultsInterface extends SearchResultsInterface
 {
     /**
-     * @return \Aligent\AsyncEvents\Api\Data\AsyncEventInterface[]
+     * @return \Aligent\AsyncEvents\Api\Data\AsyncEventDisplayInterface[]
      */
     public function getItems();
 
     /**
-     * @param \Aligent\AsyncEvents\Api\Data\AsyncEventInterface[] $items
+     * @param \Aligent\AsyncEvents\Api\Data\AsyncEventDisplayInterface[] $items
      * @return $this
      */
     public function setItems(array $items);
