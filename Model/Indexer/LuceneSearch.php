bbcd47d58ba0887d1bc9805839b0a042b80452c7
Gowri <gowri.sankar@aligent.com.au>
2022-08-31 22:30:55 +0930

    :sparkles: feat: lucene query syntax support!

---
+++ Model/Indexer/LuceneSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Aligent Consulting
+ * Copyright (c) Aligent Consulting (https://www.aligent.com.au)
+ */
+
+declare(strict_types=1);
+
+namespace Aligent\AsyncEvents\Model\Indexer;
+
+use Exception;
+use Magento\Elasticsearch\SearchAdapter\ConnectionManager;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Filters\FilterModifier;
+use Magento\Ui\Component\Filters\Type\Search;
+
+class LuceneSearch extends Search
+{
+
+    /**
+     * @var ConnectionManager
+     */
+    private $connectionManager;
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        FilterBuilder $filterBuilder,
+        FilterModifier $filterModifier,
+        ConnectionManager $connectionManager,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $filterBuilder, $filterModifier, $components, $data);
+        $this->connectionManager = $connectionManager;
+    }
+
+    /**
+     * @return void
+     */
+    public function prepare()
+    {
+        $client = $this->connectionManager->getConnection();
+        $value = $this->getContext()->getRequestParam('search');
+
+        try {
+            $rawResponse = $client->query(
+                [
+                    'index' => 'magento2_async_event_*',
+                    'q' => $value
+                ]
+            );
+
+            $rawDocuments = $rawResponse['hits']['hits'] ?? [];
+            $asyncEventIds = [];
+
+            foreach ($rawDocuments as $document) {
+                $asyncEventIds[] = $document['_id'];
+            }
+
+            if (!empty($asyncEventIds)) {
+                $filter = $this->filterBuilder->setConditionType('in')
+                    ->setField('log_id')
+                    ->setValue($asyncEventIds)
+                    ->create();
+
+                $this->getContext()->getDataProvider()->addFilter($filter);
+            }
+        } catch (Exception $exception) {
+            parent::prepare();
+        }
+    }
+}
