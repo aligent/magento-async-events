21fffc22cfbbc60133fe3d54e96a9ec606926b73
Gowri <gowrizrh@gmail.com>
2022-10-04 23:18:38 +1030

    :docs: chore: add comment explaining weird plugin

--- Plugin/MapStringScopeIdToInt.php
+++ Plugin/MapStringScopeIdToInt.php
@@ -16,6 +16,12 @@ class MapStringScopeIdToInt
     /**
      * Remap string to int
      *
+     * From Magento version 2.4.5, a new synonym filter is introduced. This is strictly typed to be an int and will
+     * fail with a PHP Type error since this module's scope is a string (the async event name). Since we do not care
+     * about stemming and synonyms as we are literally just using it for the Lucene Query Syntax we can safely ignore
+     * this and remap and provide a fake store view id of 0.
+     *
+     * @see vendor/magento/module-elasticsearch/Model/Adapter/Index/Builder.php:63
      * @param SynonymReader $subject
      * @param int|string $storeViewId
      * @return array
