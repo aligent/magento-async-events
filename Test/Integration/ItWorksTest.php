fc186ca54031d7214cb3c6e458199501884c47bb
Gowri <gowrizrh@gmail.com>
2022-10-20 14:22:40 +1030

    :green_heart: ci: add sample integration test

---
+++ Test/Integration/ItWorksTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Aligent Consulting
+ * Copyright (c) Aligent Consulting (https://www.aligent.com.au)
+ */
+
+declare(strict_types=1);
+
+namespace Aligent\AsyncEvents\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class ItWorksTest extends TestCase
+{
+    public function testItWorks()
+    {
+        $this->assertEquals(true, true);
+    }
+}
